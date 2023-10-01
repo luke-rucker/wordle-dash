a69090c39feaeb61eba1028ec7f497aca14154d9
Luke Rucker <ruckerlh@gmail.com>
2023-10-01 16:14:29 -0400

    upload sourcemaps to sentry

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,25 @@
+import { sentryVitePlugin } from '@sentry/vite-plugin'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    sentryVitePlugin({
+      org: 'wordle-dash',
+      project: 'web',
+      sourcemaps: {
+        filesToDeleteAfterUpload: '**/*.*.map',
+      },
+    }),
+  ],
   server: {
     port: 3000,
+  },
+  build: {
+    sourcemap: true,
   },
 })
